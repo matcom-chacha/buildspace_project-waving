2e2d7e00b1749a196556d1a50590bcdcf5839b99
Gaby <gabybabuchi@gmail.com>
2021-09-12 19:51:46 -0400

    Test waving to ourself

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,18 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint totalWaves;
     constructor(){
         console.log("Wiii i'm a super smart contract");
+    }
+
+    function wave() public {
+        totalWaves +=1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() view public returns(uint){
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,10 +1,20 @@
 //script to automaticaly test our smart contract by compiling, deploying and executing this
 
 async function main() {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }
 
 main()
