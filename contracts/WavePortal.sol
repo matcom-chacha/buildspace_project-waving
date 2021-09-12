f79bc568bfbb07dd0df8f35400a86e694eefdfb7
Gaby <gabybabuchi@gmail.com>
2021-09-12 18:26:56 -0400

    Initial commit

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDF-Licence-Identifier: UNLICENSED
+
+pragma solidity ^0.7.3;
+//0.8.0
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor(){
+        console.log("Wiii i'm a super smart contract");
+    }
+}
